a5b3a73c12c5039d92a1cb2b4f8a54c822e09f97
R5595 <irudhayaraj001@gmail.com>
2021-09-28 09:26:41 +0530

    ok

---
+++ src/org/ini/YesGames.java
@@ -0,0 +1,17 @@
+package org.ini;
+
+public class YesGames {
+	private void tc7() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc8() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc9() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
