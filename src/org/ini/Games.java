4744572255da0a00e93202e658ed55b9f299df62
R5595 <irudhayaraj001@gmail.com>
2021-09-28 09:06:46 +0530

    work

---
+++ src/org/ini/Games.java
@@ -0,0 +1,18 @@
+package org.ini;
+
+public class Games {
+	private void tc1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void tc3() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
